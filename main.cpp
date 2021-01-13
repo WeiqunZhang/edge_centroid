9abd688321f1dfbb3e32f5dcd9f3b1917f8479b1
Weiqun Zhang <weiqunzhang@lbl.gov>
2021-01-13 12:41:17 -0800

    use the latest capability in amrex

--- main.cpp
+++ main.cpp
@@ -42,38 +42,45 @@ int main (int argc, char* argv[])
         std::unique_ptr<EBFArrayBoxFactory> eb_fact = makeEBFabFactory(geom,ba,dm,
                                                                        {2,2,2},
                                                                        EBSupport::full);
-        Array<MultiFab,AMREX_SPACEDIM> edge_centroid;
-        for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
-            IntVect index_type{1};
-            index_type[idim] = 0;  // All nodal except idim-direction
-            edge_centroid[idim].define(amrex::convert(ba,index_type), dm, 1, 1, MFInfo{}, *eb_fact);
-        }
 
-        amrex::FillEdgeCentroid(GetArrOfPtrs(edge_centroid));
+        auto const& flags = eb_fact->getMultiEBCellFlagFab();
+        auto const& edge_centroid = eb_fact->getEdgeCent();
 
-        for (MFIter mfi(edge_centroid[0]); mfi.isValid(); ++mfi) {
-            for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
-                auto const& edge = edge_centroid[idim].const_array(mfi);
-                amrex::LoopOnCpu(Box(edge), [=] (int i, int j, int k)
-                {
-                    if (edge(i,j,k) == Real(-1.0)) {
-                        // This edge is all covered
-                    } else if (edge(i,j,k) == Real(1.0)) {
-                        // This edge is all open
-                    } else {
-                        // This edge is cut.
-                        Real edge_cent = edge(i,j,k); // edge centroid: (-0.5,0.5)
-                        if (edge_cent < Real(0.0)) {
-                            // The right side is covered.
-                            Real cut_position = Real(2.0)*edge_cent + Real(0.5); // (-0.5,0.5)
+        for (MFIter mfi(flags); mfi.isValid(); ++mfi) {
+            Box const& box = mfi.validbox();
+            FabType fab_type = flags[mfi].getType(box);
+            if (fab_type == FabType::regular) {
+                // every cell in box is all regular
+            } else if (fab_type == FabType::covered) {
+                // every cell in box is all covered
+            } else {
+                // mix of regular, cut and covered
+                // Note that edge_centroid[idim] is a MultiCutFab that does not have
+                // data if the fab type regular or covered.
+                for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
+                    auto const& edge = edge_centroid[idim]->const_array(mfi);
+                    amrex::LoopOnCpu(amrex::convert(box,Box(edge).ixType()),
+                    [=] (int i, int j, int k)
+                    {
+                        if (edge(i,j,k) == Real(-1.0)) {
+                            // This edge is all covered
+                        } else if (edge(i,j,k) == Real(1.0)) {
+                            // This edge is all open
                         } else {
-                            // The left side is covered
-                            Real cut_position = Real(2.0)*edge_cent - Real(0.5); // (-0.5,0.5)
+                            // This edge is cut.
+                            Real edge_cent = edge(i,j,k); // edge centroid: (-0.5,0.5)
+                            if (edge_cent < Real(0.0)) {
+                                // The right side is covered.
+                                Real cut_position = Real(2.0)*edge_cent + Real(0.5); // (-0.5,0.5)
+                            } else {
+                                // The left side is covered
+                                Real cut_position = Real(2.0)*edge_cent - Real(0.5); // (-0.5,0.5)
+                            }
+                            Real cut_poisition = Real(2.0)*edge_cent
+                                - std::copysign(Real(0.5), edge_cent); // (-0.5, 0.5)
                         }
-                        Real cut_poisition = Real(2.0)*edge_cent
-                            - std::copysign(Real(0.5), edge_cent); // (-0.5, 0.5)
-                    }
-                });
+                    });
+                }
             }
         }
     }
